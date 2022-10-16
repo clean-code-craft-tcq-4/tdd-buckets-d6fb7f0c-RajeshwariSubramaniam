a654607bd8bc708f720c05ecd1a2f89a71056584
Rajeshwari Subramaniam <Subramaniam.Rajeshwari@in.bosch.com>
2022-10-16 16:39:16 +0530

    refactor code

--- src/charging_current_ranges.py
+++ src/charging_current_ranges.py
@@ -3,7 +3,7 @@ import itertools
 
 def input_for_consecutive_check(current_readings):
     value = []
-    for i in range(15):
+    for i in range(min(current_readings), max(current_readings)+1):
         if i in current_readings:
             value.append(i)
             continue
@@ -11,8 +11,8 @@ def input_for_consecutive_check(current_readings):
     return value
 
 
-def get_consecutive_groups(current_counts):
-    return [list(y) for x, y in itertools.groupby(current_counts, lambda z: z == 0) if not x]
+def get_consecutive_groups(values):
+    return [list(y) for x, y in itertools.groupby(values, lambda z: z == 0) if not x]
 
 
 def current_ranges_and_count(current_readings):
@@ -25,10 +25,15 @@ def current_ranges_and_count(current_readings):
             occurrences += current_readings.count(element)
         between_range = str(min(consecutive_samples[i])) + '-' + str(max(consecutive_samples[i]))
         ranges_count.append((between_range, str(occurrences)))
-    string_format = ""
-    for item in ranges_count:
-        string_format += "{}, {}".format(item[0], item[1])
-    return string_format
+    return ranges_count
+
+
+def output_to_csv_format(current_readings):
+    ranges = current_ranges_and_count(current_readings)
+    csv_string_format = "Range, Readings\n"
+    for item in ranges:
+        csv_string_format += "{}, {}\n".format(item[0], item[1])
+    return csv_string_format
 
 
 if __name__ == '__main__':
