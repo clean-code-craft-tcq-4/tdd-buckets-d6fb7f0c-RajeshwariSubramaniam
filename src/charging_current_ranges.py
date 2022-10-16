aa60f30ef46524b4212628c5a4fa00aa796cc4fa
Rajeshwari Subramaniam <Subramaniam.Rajeshwari@in.bosch.com>
2022-10-16 16:01:37 +0530

    implement the ranges functionality

---
+++ src/charging_current_ranges.py
@@ -0,0 +1,32 @@
+import itertools
+
+
+def input_for_consecutive_check(current_readings):
+    value = []
+    for i in range(15):
+        if i in current_readings:
+            value.append(i)
+            continue
+        value.append(0)
+    return value
+
+
+def get_consecutive_groups(current_counts):
+    return [list(y) for x, y in itertools.groupby(current_counts, lambda z: z == 0) if not x]
+
+
+def current_ranges_and_count(current_readings):
+    samples = input_for_consecutive_check(current_readings)
+    consecutive_samples = get_consecutive_groups(samples)
+    ranges_count = []
+    for i in range(len(consecutive_samples)):
+        occurrences = 0
+        for element in consecutive_samples[i]:
+            occurrences += current_readings.count(element)
+        between_range = str(min(consecutive_samples[i])) + '-' + str(max(consecutive_samples[i]))
+        ranges_count.append((between_range, str(occurrences)))
+    return ranges_count
+
+
+if __name__ == '__main__':
+    current_ranges_and_count([4, 5])
